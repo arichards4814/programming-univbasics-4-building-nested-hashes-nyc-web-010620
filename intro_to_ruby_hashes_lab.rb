59561931330f54b538e6ceee3cefbf9373855f4a
Andrew Richards <arichards4814@gmail.com>
2019-10-24 18:57:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
     "mortgage_value" => "$400"
   },
   :shortline_railroad =>{
-    "mortgage_value" => "$500"
+    "mortgage_value" => "$800"
   }},
   :rent_in_dollars =>{:one_piece_owned => 25,:two_pieces_owned =>50,:three_pieces_owned =>100,:four_pieces_owned =>200}
     
